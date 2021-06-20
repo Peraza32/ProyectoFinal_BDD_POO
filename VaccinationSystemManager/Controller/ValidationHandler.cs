fd6646f8683f81ad84d9a50bd401f5a10b65f4b7
DieR-G <00043920@uca.edu.sv>
2021-06-19 22:30:44 -0600

    login finished

---
+++ VaccinationSystemManager/Controller/ValidationHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationSystemManager.Controller
+{
+    public abstract class ValidationHandler : IValidator
+    {
+        private ValidationHandler? next = null;
+        public ValidationHandler(ValidationHandler? next)
+        {
+            SetNext(next);
+        }
+        public virtual bool Validate()
+        {
+            //if its the last handler on the chain just returns true
+            if(next is null)
+            {
+                return true;
+            }
+            //otherwise it checks the validation in the next handler
+            else
+            {
+                return next.Validate();
+            }
+        }
+        public void SetNext(ValidationHandler next)
+        {
+            //if there is no next handler sets it to the argument received
+            if(this.next is null)
+            {
+                this.next = next;
+            }
+            //if there is then it appends to the chain
+            else
+            {
+                next.SetNext(next);
+            }
+        }
+    }
+}
