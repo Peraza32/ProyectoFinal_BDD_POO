08903ed8ee9d8416e4f5dbdf93ad1b941b765218
Victor <victorperaza640@gmail.com>
2021-06-21 01:53:56 -0600

    Base for Appointment

---
+++ VaccinationSystemManager/Controller/Appointment/AppointmentMaker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccinationSystemManager.Model;
+
+namespace VaccinationSystemManager.Controller.Appointment
+{
+    class AppointmentMaker:IAppointment
+    {
+        public bool MakeAppointment(DoseType dose)
+        {
+
+            return true;
+        
+        }
+
+
+        private DateTime GenerateDate()
+        {
+
+            return;
+        }
+
+
+    }
+}
---
+++ VaccinationSystemManager/Controller/Appointment/IAppointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccinationSystemManager.Model;
+
+namespace VaccinationSystemManager.Controller
+{
+    interface IAppointment
+    {
+        public bool MakeAppointment(DoseType dose);
+    }
+}
