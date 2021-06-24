3e512a0d0e0142be690dee0650288e346cbbd4e6
Ernesto-Canales <00051120@uca.edu.sv>
2021-06-24 01:07:07 -0600

    Finishing Tab1 working Chart

---
+++ VaccinationSystemManager/Controller/Statistics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationSystemManager.Controller
+{
+    public static class Statistics
+    {
+        //Count Vaccination Process
+        public static int CountVaccinationProcess(int countShotType)
+        {
+            int count;
+
+            using (Model.G4ProyectoDBContext db = new Model.G4ProyectoDBContext())
+            {
+                count = db.VaccinationProcesses
+                    .Where(u => u.ShotType == countShotType)
+                    .Count();
+            }
+
+            return count;
+        }
+    }
+}
---
+++ VaccinationSystemManager/Views/frmStatistics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+
+namespace VaccinationSystemManager.Views
+{
+    public partial class frmStatistics : Form
+    {
+        public frmStatistics()
+        {
+            InitializeComponent();
+        }
+
+        private void frmStatistics_Load(object sender, EventArgs e)
+        {
+            //Pie chart for Tab1 == "Personas vacunadas en total, solo con una dosis y con ambas dosis"
+            CreateStaticPieTab1();
+
+            //Pie chart for Tab2 == "Efectos secundarios presentados"
+            //CreateStaticPieTab2();
+
+            //Rectangle Bar Serie for Tab3 == "Eficiencia del proceso de vacunación"
+            //CreateStaticPieTab3();
+        }
+
+        //Functions to create Charts
+        public void CreateStaticPieTab1()
+        {
+            var modelP1 = new PlotModel { Title = "Personas vacunadas en total" };
+
+            dynamic seriesP1 = new PieSeries { StrokeThickness = 2.0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
+
+            seriesP1.Slices.Add(new PieSlice("Primera dosis", Controller.Statistics.CountVaccinationProcess(1)) { IsExploded = true, Fill = OxyColor.FromRgb(243, 156, 18) });
+            seriesP1.Slices.Add(new PieSlice("Segunda dosis", Controller.Statistics.CountVaccinationProcess(2)) { IsExploded = true, Fill = OxyColor.FromRgb(39, 174, 96) });
+
+            modelP1.Series.Add(seriesP1);
+            plotView1.Model = modelP1;
+        }
+    }
+}
